83e9ffa7dd4062ca55b668f7df636b86ffb8040e
Vinayak <myselfvinayakhegde@gmail.com>
2024-01-15 23:20:32 +0530

    add queue notes

--- CustomDS/Queue/index.js
+++ CustomDS/Queue/index.js
@@ -1,4 +1,12 @@
 // Queue - Sequential collection of elements that follow FIFO
-// Enter from rear/tail leave from fron/head
+// Enter from rear/tail leave from front/head
 // It is an abstract data type. It is defined by its behavior rather than being a mathematical model.
+// Two main operations Enqueue and Deque
+// Enqueue - adds at end of the collection
+// Deque - remove from front
+
+// Usage , when u have to process in an orderly fashion
+// Callback queue in JS runtime
+// CPU task scheduling
+// Printers
 
